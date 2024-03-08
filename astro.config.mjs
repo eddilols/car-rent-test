e63c820af23e571f3e7391fb87fd3db806d1a8f7
eddilols <46637323+eddilols@users.noreply.github.com>
2024-03-08 05:32:10 +0300

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
+  output: "server",
+  adapter: netlify()
 });
